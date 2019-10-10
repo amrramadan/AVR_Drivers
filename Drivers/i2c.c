4473ec67d2651d6a38ccd8e0daadf8b139d560a2
amr ramadan <amrramadan1@outlook.com>
2019-10-10 18:44:31 +0200

    I2C Driver

---
+++ Drivers/i2c.c
@@ -0,0 +1,11 @@
+/*
+ * Module: i2c.c
+ * Description: source file for I2C Driver
+ *  Created on: Oct 10, 2019
+ *      Author: Amr Ramadan
+ */
+
+#include "i2c.h"
+
+
+
---
+++ Drivers/i2c.h
@@ -0,0 +1,19 @@
+/*
+ * Module: i2c.h
+ * Description: Header file for I2C Driver
+ *  Created on: Oct 10, 2019
+ *      Author: Amr Ramadan
+ */
+
+
+
+#include "common_macros.h"
+#include "std_types.h"
+#include "micro_config.h"
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+#endif /* I2C_H_ */
