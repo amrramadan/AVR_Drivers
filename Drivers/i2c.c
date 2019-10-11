fe7a23fd2539a637ff6a7e5bffd19292740c9417
amr ramadan <amrramadan1@outlook.com>
2019-10-11 23:47:14 +0200

    Update

--- Drivers/i2c.c
+++ Drivers/i2c.c
@@ -7,5 +7,74 @@
 
 #include "i2c.h"
 
+void TWI_init(void) {
+	TWCR = (1 << TWEN); // Enable TWI
+	//TWSR = 0x00 ; // TWPS1:0 = 00 >> Prescaller Value
+	TWBR = 0x02; // Bit Rate: 400.000 kbps  ( Prescaller 00 and F_CPU = 8000000 hz)
+	//TWDR = 0;  // Empty Data Register
+	TWAR = 0b0000001; // Initiate First Device Address (7-bits Address)
+}
 
+void TWI_START(void) {
+/*
+ * TWEN  = 1 : Enable TWI Protocol
+ * TWINT = 1 : Clear Interrupt Flag
+ * TWSTA = 1 : Send Start Bit
+ */
+	TWCR = (1<<TWEN) | (1<<TWINT) | (1<<TWSTA);
+	/*wait until Start bit send successfully*/
+	while(BIT_IS_CLEAR(TWCR,TWINT));
+}
+
+void TWI_stop(void)
+{
+	/*
+	 * TWEN  = 1 : Enable TWI Protocol
+	 * TWINT = 1 : Clear Interrupt Flag
+	 * TWSTA = 1 : Send Stop Bit
+	 */
+		TWCR = (1<<TWEN) | (1<<TWINT) | (1<<TWSTO);
+}
+
+void TWI_Write(uint8 Data){
+	// initiate Data Register With Data
+	TWDR = Data;
+	/*
+	 * TWEN  = 1 : Enable TWI Protocol
+	 * TWINT = 1 : Clear Interrupt Flag
+	 */
+
+	TWCR = (1<<TWEN) | (1<<TWINT);
+	/*wait until data is send successfully*/
+	while(BIT_IS_CLEAR(TWCR,TWINT));
+}
+
+uint8 TWI_readWithACK(void){
+	/*
+	 * TWEN  = 1 : Enable TWI Protocol
+	 * TWINT = 1 : Clear Interrupt Flag
+	 * TWEA  = 1 : Enable Acknowledge Bit
+	 */
+	TWCR = (1<<TWEN) | (1<<TWINT) | (1<<TWEA);
+	/*wait until Ack is received successfully*/
+	while(BIT_IS_CLEAR(TWCR,TWINT));
+	return TWDR; //Read Data
+
+}
+uint8 TWI_readWithNACK(void){
+	/*
+	 * TWEN  = 1 : Enable TWI Protocol
+	 * TWINT = 1 : Clear Interrupt Flag
+	 */
+	TWCR = (1<<TWEN) | (1<<TWINT);
+	/*wait until data is received successfully*/
+	while(BIT_IS_CLEAR(TWCR,TWINT));
+	return TWDR; //Read Data
+}
+
+uint8 TWI_getStatus(void){
+	uint8 status;
+	status = TWSR & 0xF8;
+	return status;
+}
 
--- Drivers/i2c.h
+++ Drivers/i2c.h
@@ -14,6 +14,21 @@
 #ifndef I2C_H_
 #define I2C_H_
 
+/*******************Preprocessor Macros*************************/
 
+/*TWSR Register status BITS*/
+#define TW_START 		0x08 //start condition has been transmitted
+#define TW_REP_START	0x10 //repeated start condition has been transmitted
+#define TW_MT_SLA_W_ACK	0x18 //master transmitted (slave address + write request) to slave and Ack received from slave
+#define TW_MT_SLA_R_ACK 0x40 //master transmitted (slave address + read request) to slave and Ack received from slave
+
+/*******************Functions Prototypes*************************/
+void TWI_init(void);
+void TWI_START(void);
+void TWI_stop(void);
+void TWI_Write(uint8 Data);
+uint8 TWI_readWithACK(void);
+uint8 TWI_readWithNACK(void);
+uint8 TWI_getStatus(void);
 
 #endif /* I2C_H_ */
