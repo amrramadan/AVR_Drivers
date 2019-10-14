19b1bc8c73edaa5db21707bb586601dfe4852a07
amr ramadan <amrramadan1@outlook.com>
2019-10-14 11:45:26 +0200

    update

--- Challenge_SPI_MC1/mc1.c
+++ Challenge_SPI_MC1/mc1.c
@@ -7,8 +7,17 @@
 
 
 #include "spi.h"
-
+#include "keypad.h"
 int main(){
-
+	uint8 var;
+	SPI_initMaster(Fosc_4);
+   KEYPAD_PORT_DIR = 0x00; //configure keypad switches as input pins
+   KEYPAD_PORT_IN = 0xFF;
+	while(1)
+	{
+		var=Keypad_getPressedKey();
+		SPI_sendByte(var);
+		_delay_ms(500);
+	}
 
 }
--- Challenge_SPI_MC2/mc2.c
+++ Challenge_SPI_MC2/mc2.c
@@ -7,8 +7,17 @@
 
 
 #include "spi.h"
-
+#include "lcd.h"
 int main(){
+	uint8 var;
+	LCD_init();
+	SPI_initSlave();
 
+	while(1){
+		var=SPI_receiveByte();
+		if(var >=0 && var <=9)
+			LCD_integerToString(var);
+		else LCD_displayChar(var);
+	}
 
 }
